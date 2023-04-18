6526e5b380c09ff70088d4cef6fc524ab92defe5
Konrad Romański <konrad.romanski4@gmail.com>
2023-04-18 21:32:45 +0200

    Partialy working image encryption

--- advanced/image_encryption/ImageCiphering.py
+++ advanced/image_encryption/ImageCiphering.py
@@ -1,37 +1,39 @@
+import numpy as np
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+
 class ImageCiphering:
-    def encrypt_image(self, image, array):
-        for i in range(len(image)):
-            for j in range(len(image[i])):
-                for k in range(len(image[i][j])):
-                    if j % 3 == 0:
-                        image[i][j][k] = array[image[i][j][k]]
-                    elif j % 3 == 1:
-                        image[i][j][k] = array[(image[i][j][k] + i) % 256]
-                    else:
-                        image[i][j][k] = array[(image[i][j][k] + j) % 256]
-        return image
+    def __init__(self, key):
+        self.key = key
 
-    def decrypt_image(self, image, array):
-        # Compute the inverse of the encryption array
-        inverse_array = [0] * 256
-        for i in range(256):
-            inverse_array[array[i]] = i
+    def encrypt_image(self, image):
+        nrows, ncols, nchannels = image.shape
+        padded_image = np.pad(image, [(0, nrows % 16), (0, ncols % 16), (0, 0)], mode='edge')
+        flattened_image = padded_image.flatten()
 
-        # Decrypt the image by reversing the encryption process
-        for i in range(len(image)):
-            for j in range(len(image[i])):
-                r, g, b = image[i][j]
-                if j % 3 == 0:
-                    image[i][j] = [inverse_array[r], inverse_array[g], inverse_array[b]]
-                elif j % 3 == 1:
-                    r = (r - i) % 256
-                    g = (g - i) % 256
-                    b = (b - i) % 256
-                    image[i][j] = [inverse_array[r], inverse_array[g], inverse_array[b]]
-                else:
-                    r = (r - j) % 256
-                    g = (g - j) % 256
-                    b = (b - j) % 256
-                    image[i][j] = [inverse_array[r], inverse_array[g], inverse_array[b]]
+        # Encrypt the data using AES
+        cipher = Cipher(algorithms.AES(self.key), modes.ECB(), backend=default_backend())
+        encryptor = cipher.encryptor()
+        encrypted_data = encryptor.update(flattened_image) + encryptor.finalize()
 
-        return image
+        # Reshape the encrypted data to match the padded image
+        encrypted_image = np.frombuffer(encrypted_data, dtype=np.uint8).reshape(padded_image.shape)
+
+        return encrypted_image
+
+    def decrypt_image(self, encrypted_image):
+        nrows, ncols, nchannels = encrypted_image.shape
+        flattened_image = encrypted_image.flatten()
+
+        # Decrypt the data using AES
+        cipher = Cipher(algorithms.AES(self.key), modes.ECB(), backend=default_backend())
+        decryptor = cipher.decryptor()
+        decrypted_data = decryptor.update(flattened_image) + decryptor.finalize()
+
+        # Remove padding and reshape the decrypted data to match the original image
+        decrypted_data = decrypted_data[:nrows * ncols * nchannels]
+        decrypted_image = np.frombuffer(decrypted_data, dtype=np.uint8).reshape((nrows, ncols, nchannels))
+
+        return decrypted_image
+
--- advanced/image_encryption/main.py
+++ advanced/image_encryption/main.py
@@ -1,31 +1,35 @@
 from DataReader import DataReader
 from ImageCiphering import ImageCiphering
 import numpy as np
-
+import os
 
 def main():
     # Init objects
     data = DataReader("sbox.SBX")
     # image = DataReader("img.png")
     image = DataReader("pikatchu.png")
-    image_ciphering = ImageCiphering()
 
     # Read data
     encryption_array = data.read_binary_data()
+    print(encryption_array)
+
+
+    key = os.urandom(32)  # 256-bit key
+
+    sbox = np.arange(256)
+    np.random.shuffle(sbox)
+    image_ciphering = ImageCiphering(key)
 
     # Encrypt image using encryption array and save it
     encrypted_image = image.read_image()
-    # encrypted_image = image_ciphering.multiple_rounds_encryption(encrypted_image, encryption_array)
-    encrypted_image = image_ciphering.encrypt_image(encrypted_image, encryption_array)
+
+    encrypted_image = image_ciphering.encrypt_image(encrypted_image)
     print("Image encrypted")
-    # for i in range(5):
-    #     encrypted_image = image_ciphering.encrypt_image(encrypted_image, encryption_array)
-    #     encryption_array = [int((i + 2)/2) for i in encryption_array]
-    # data.save_image(encrypted_image, "encrypted_img.png")
+
     data.save_image(np.array(encrypted_image, dtype=np.uint8), "pikatchu_en.png")
 
     # # Decrypt encrypted image and save it
-    decrypted_image = image_ciphering.decrypt_image(encrypted_image, encryption_array)
+    decrypted_image = image_ciphering.decrypt_image(encrypted_image)
     data.save_image(np.array(decrypted_image, dtype=np.uint8), "pikatchu_de.png")
     print("Image decrypted")
 
