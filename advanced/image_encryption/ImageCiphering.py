7d170e679f6333f9de868ebb4f380b5c9c03e48a
Konrad Romański <konrad.romanski4@gmail.com>
2023-04-18 22:14:42 +0200

    Finally working encryption & decryption

--- advanced/image_encryption/ImageCiphering.py
+++ advanced/image_encryption/ImageCiphering.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pickle
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
@@ -9,31 +10,58 @@ class ImageCiphering:
 
     def encrypt_image(self, image):
         nrows, ncols, nchannels = image.shape
-        padded_image = np.pad(image, [(0, nrows % 16), (0, ncols % 16), (0, 0)], mode='edge')
+        pad_rows = int(np.ceil(nrows / 16.0) * 16) - nrows
+        pad_cols = int(np.ceil(ncols / 16.0) * 16) - ncols
+        padded_image = np.pad(image, [(0, pad_rows), (0, pad_cols), (0, 0)], mode='edge')
         flattened_image = padded_image.flatten()
 
-        # Encrypt the data using AES
+        # Shuffle the pixels
+        permutation = np.random.permutation(len(flattened_image))
+        shuffled_image = flattened_image[permutation]
+
+        # Encrypt the shuffled data using AES
         cipher = Cipher(algorithms.AES(self.key), modes.ECB(), backend=default_backend())
         encryptor = cipher.encryptor()
-        encrypted_data = encryptor.update(flattened_image) + encryptor.finalize()
+        encrypted_data = encryptor.update(shuffled_image) + encryptor.finalize()
 
         # Reshape the encrypted data to match the padded image
         encrypted_image = np.frombuffer(encrypted_data, dtype=np.uint8).reshape(padded_image.shape)
 
+        # Serialize the permutation and padded image shape for decryption
+        permutation_list = permutation.tolist()
+        self.permutation_data = pickle.dumps((permutation_list, padded_image.shape))
+
         return encrypted_image
 
     def decrypt_image(self, encrypted_image):
-        nrows, ncols, nchannels = encrypted_image.shape
-        flattened_image = encrypted_image.flatten()
+        # Deserialize the permutation and padded image shape
+        permutation_list, padded_image_shape = pickle.loads(self.permutation_data)
 
-        # Decrypt the data using AES
+        # Decrypt the shuffled data using AES
         cipher = Cipher(algorithms.AES(self.key), modes.ECB(), backend=default_backend())
         decryptor = cipher.decryptor()
-        decrypted_data = decryptor.update(flattened_image) + decryptor.finalize()
+        decrypted_data = decryptor.update(encrypted_image.flatten()) + decryptor.finalize()
 
-        # Remove padding and reshape the decrypted data to match the original image
-        decrypted_data = decrypted_data[:nrows * ncols * nchannels]
-        decrypted_image = np.frombuffer(decrypted_data, dtype=np.uint8).reshape((nrows, ncols, nchannels))
+        # Unshuffle the pixels
+        unshuffled_data = np.zeros(len(permutation_list), dtype=np.uint8)
+        for i, j in enumerate(permutation_list):
+            unshuffled_data[j] = decrypted_data[i]
 
-        return decrypted_image
+        # Reshape the unshuffled data to match the padded shape of the original image
+        unshuffled_image = unshuffled_data.reshape(padded_image_shape)
+
+        # Remove the padded pixels from the unshuffled image to get the original image
+        nrows, ncols, nchannels = padded_image_shape
+        pad_rows = int(np.ceil(nrows / 16.0) * 16) - nrows
+        pad_cols = int(np.ceil(ncols / 16.0) * 16) - ncols
+        original_image = unshuffled_image[:nrows - pad_rows, :ncols - pad_cols, :]
+
+        return original_image
+
+
+
+
+
+
+
 
